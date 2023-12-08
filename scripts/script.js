fb97ee20fd19b2a9b60c3de5472367d9f7b771d9
TristanKatte <tristankatte@hotmail.com>
2023-12-08 10:17:17 +0100

    alles werkend gemaakt

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
 var button = document.querySelector("button");
-var element = document.querySelector("div");
+var element = document.querySelector("div.article-content");
+var info = document.querySelector("div.info");
 
+function showcontent(){
+    info.classList.toggle("show");
+}
+
+button.addEventListener("click", showcontent );
 button.addEventListener("click", function() {
     element.classList.toggle("article-content-expanded");
    
